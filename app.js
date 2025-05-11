6723e9df3a2ec36eee23ec8c74784186e8e990b1
ASM Tamzid <atkthegreat9999@gmail.com>
2025-05-12 00:04:20 +0600

    added html

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const userRouter = require('./routes/rou_user.js');
+const downloadRouter = require('./routes/rou_download.js');
 const {checkUserAgent} = require('./middLeware/mid_user.js');
 const auditLogger = require('./middLeware/mid_auditLogger.js');
 const logUserAgent = require('./middLeware/logUserAgent.js')
+const path = require('path');
 
 app.use(express.json());
 
 app.use(logUserAgent)
 
 app.use('/api',checkUserAgent, auditLogger, userRouter);
+app.use('/ap',downloadRouter);
 
+app.use(express.static('views'));
+app.get('/analytics', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/analytics.html'));
+});
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
---
+++ routes/rou_download.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const {gerUserAgentAnalytics} = require('../controllers/con_analytics.js');
+const {getAll} = require('../controllers/con_user.js');
+const fs = require('fs');
+const path = require('path');
+
+router.get('/user-agent', gerUserAgentAnalytics);
+
+router.get('/download', (req,res)=>{
+    const logFilePath = path.join(__dirname,'../userAgent.json');
+    const logs = JSON.parse(fs.readFileSync(logFilePath, 'utf8'));
+
+    let csv = 'User Agent, Timestamp\n';
+    logs.forEach((entry) => {
+        csv += `${entry.userAgent}, ${entry.timestamp}\n`;
+    });
+
+    res.setHeader('Content-disposition', 'attachment; filename=userAgent.csv');
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(csv);
+
+});
+
+module.exports = router;
